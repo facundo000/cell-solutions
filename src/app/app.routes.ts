d2e1fa3d4a1aae6d9f7d33a947161c8c95fdd4d2
facundo000 <facundo.guzman@mi.unc.edu.ar>
2025-03-17 18:58:49 -0300

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { HeaderComponent } from './shared/header/header.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: 'home' },
+    { path: 'home', component: HeaderComponent },
+];
